96bdf95bb1553a734c1ca78b24e6489f9ba8e481
= <=>
2025-11-25 18:07:19 -0500

    Extend to basic Chat UI

--- src/app/api/completion/route.ts
+++ src/app/api/completion/route.ts
@@ -1,11 +1,19 @@
 import { openai } from "@ai-sdk/openai";
 import { generateText } from "ai";
 
-export async function POST(){
+export async function POST(req: Request){
+
+  try {
+    const {prompt} = await req.json();
+
   const {text} = await generateText({
     model: openai("gpt-4.1-nano"),
-    prompt: "Explain an LLM in simple terms"
+    prompt
   })
 
   return Response.json({text})
+  } catch (error) {
+    console.error("Error geenrating text", error);
+    return Response.json({error: "Failed to geenrate text"}, {status: 500})
+  }
 }//POST
